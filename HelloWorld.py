6be9ac143ffbc7933f9457124fbe880ead14e38b
qlqcoisa@gmail.com <qlqcoisa@gmail.com>
2024-09-19 07:57:13 -0300

    atualizando commit part.2

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,3 @@
 print ('Hello World')
 print ('Esse é um projeto de avaliação sobre Gerenciamento de Repositórios!')
+print ('Docente: Daniel Enos Cavalcanti Rodrigues de Macedo; Discente: Giulya Buriti de Araújo Azevedo.')
